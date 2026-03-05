00d42a4ea3ee770a8856c328736d20eb4ed3882c
thaynaralimaa61@gmail.com <thaynaralimaa61@gmail.com>
2026-03-05 19:23:59 -0300

    feat: add 404 middleware in server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,3 +4,9 @@ const PORT = 3000
 const app = express();
 
 app.listen(PORT, () => console.log(`Server running at ${PORT}`))
+
+app.use((req, res) => {
+    res.status(404).json({
+        message: 'Not found'
+    })
+})
