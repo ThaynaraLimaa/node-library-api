220291a27bd8b80ed713ebfc68cfbf6c03d15eb3
thaynaralimaa61@gmail.com <thaynaralimaa61@gmail.com>
2026-03-05 19:48:17 -0300

    feat: connect book router to main server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { bookRouter } from './routes/book.routes.js';
 
 const PORT = 3000
 const app = express();
 
 app.listen(PORT, () => console.log(`Server running at ${PORT}`))
+
+app.use('/books', bookRouter)
 
 app.use((req, res) => {
     res.status(404).json({
